bcb026a63ad6ad4a45ca93c29877278b39c6f91b
Andy Perrett <43847848+18684092@users.noreply.github.com>
2019-06-27 21:07:24 +0100

    Update ROSCallback.cpp

--- src/ROSCallback.cpp
+++ src/ROSCallback.cpp
@@ -4,8 +4,8 @@
 
 bool ROSCallback::ROSValidDir(rosmaze::MazeController::Request &req, rosmaze::MazeController::Response &res)
 {
-    // request is a string (direction)
-    // responce is a string (result)
+    // request is a int64 (direction)
+    // responce is a int64 (result)
     res.result = 0;
     ROS_INFO("request: %d", (int)req.direction);
     ROS_INFO("sending back response: %d", (int)res.result);
