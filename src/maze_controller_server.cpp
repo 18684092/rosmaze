40cb958c68380229d36573180a007a7b1b0034fe
18684092 <18684092@students.lincoln.ac.uk>
2019-06-27 19:05:36 +0100

    Removing old code

--- src/maze_controller_server.cpp
+++ src/maze_controller_server.cpp
@@ -7,25 +7,10 @@ Course: BSc Computer Science Level 1
 
 // My classes
 #include "RecursiveBackTracker.h"
-#include "ROSCallback.h"
-
-// ROS 
-#include "ros/ros.h"
-#include "rosmaze/MazeController.h"
-
-bool R(rosmaze::MazeController::Request &req, rosmaze::MazeController::Response &res)
-{
-    // request is a int (direction)
-    // responce is a int (result)
-    res.result = 1;
-    ROS_INFO("request: %d", (int)req.direction);
-    ROS_INFO("sending back response: %d", (int)res.result);
-    return true;
-}
 
 int main(int argc, char **argv)
 {
-    
+    // Initialise ROS
     ros::init(argc, argv, "maze_controller_server");
     // Set up size of maze y, x - needs to fit into screen 13 x 17 good for 640 by 480
     RecursiveBackTracker BT(13,17);
